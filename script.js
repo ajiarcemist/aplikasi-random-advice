54c96aa33669c6adada771878af1e77d04e75799
ajiarcemist <ajiarcemist@gmail.com>
2024-05-19 12:43:05 +0700

    Perbaikan script karena tidak merandom

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
 const adviceId = document.querySelector('#adviceId');
 const adviceText = document.querySelector('#advicetext');
 const btn = document.querySelector('#btn');
-function getData(adviceData){
-        const adviceNum = adviceData.slip.id;
-        const advice = adviceData.slip.advice;
-    
-        adviceId.textContent = adviceNum;
-        adviceText.innerHTML = `<p>${advice}</p>`;
-    }
 
-function getAdvice(){
-fetch('https://api.adviceslip.com/advice').then(response => {
-    return response.json();
-}).then((value)=>{
-    btn.innerHTML = '<img src="./images/icon-dice.svg" alt=""></img>'
-    getData(value)
-}).catch(error => {
-    console.log(error);
-})
+function getData(adviceData) {
+    const adviceNum = adviceData.slip.id;
+    const advice = adviceData.slip.advice;
+
+    adviceId.textContent = adviceNum;
+    adviceText.innerHTML = `<p>${advice}</p>`;
 }
 
-btn.addEventListener('click', async function(){
-   let isLoading = true
-   btn.innerText = "Loading..."
-    await getAdvice();
-   
-})
-function saveToLocalStorage(id, advice){
+function saveToLocalStorage(id, advice) {
     localStorage.setItem('id', id);
     localStorage.setItem('advice', advice);
 }
 
-function getItem(){
+function getAdvice() {
+    return fetch('https://api.adviceslip.com/advice')
+        .then(response => response.json())
+        .then(value => {
+            const adviceData = value.slip;
+            saveToLocalStorage(adviceData.id, adviceData.advice);
+            return adviceData;
+        });
+}
+
+btn.addEventListener('click', async function() {
+    btn.innerText = "Loading...";
+    try {
+        const adviceData = await getAdvice();
+        getData(adviceData);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        btn.innerHTML = '<img src="./images/icon-dice.svg" alt=""></img>';
+    }
+});
+
+function getItem() {
     return {
         slip: {
-          id: localStorage.getItem('id'),
-          advice: localStorage.getItem('advice')
+            id: localStorage.getItem('id'),
+            advice: localStorage.getItem('advice')
         }
-      
-      }
-    }
-window.onload = () => {
-    saveToLocalStorage('117', "It is easy to sit up and take notice, what's difficult is getting up and taking action.")
-    getData(getItem());
+    };
 }
+
+window.onload = () => {
+    const savedAdvice = getItem();
+    if (savedAdvice.slip.id && savedAdvice.slip.advice) {
+        getData(savedAdvice);
+    } else {
+        getAdvice().then(adviceData => getData(adviceData));
+    }
+};
